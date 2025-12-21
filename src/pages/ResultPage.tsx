922757e5757c01ca5bc89741ce4664ad87e4aaf6
twentyvanty <lovelyzkim4@gmail.com>
2025-12-21 23:58:40 +0700

    Do mock test case generate template

--- src/pages/ResultPage.tsx
+++ src/pages/ResultPage.tsx
@@ -9,8 +9,17 @@ function ResultPage({ requirement, testType }: ResultPageProps) {
         <div>
             <h1>Result</h1>
             <p>Requirement: {requirement}</p>
-            <p>Test Type: {testType === 'BDD' && 'BDD' || testType === 'TDD' && 'TDD'}</p>
+            {/* <p>Test Type: {testType === 'BDD' && 'BDD' || testType === 'TDD' && 'TDD'}</p> */}
             {/* if test type = bdd then show bdd or show tdd if  = tdd */}
+            <div>
+                {testType === 'TDD' && (
+                    <p>Requirement: {requirement}
+                    Red: ... <br />
+                    Gree: ... <br />
+                    Refactor: ... <br />
+                    </p>
+                )}
+            </div>
         </div>
     )
 }
