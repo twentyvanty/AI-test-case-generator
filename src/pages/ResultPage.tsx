4d6c9c9083b38b4fd59fe29b1269ea391899dbbe
twentyvanty <lovelyzkim4@gmail.com>
2025-12-21 23:26:42 +0700

    add test type condition

--- src/pages/ResultPage.tsx
+++ src/pages/ResultPage.tsx
@@ -4,11 +4,13 @@ type ResultPageProps = {
 }
 
 function ResultPage({ requirement, testType }: ResultPageProps) {
+
     return (
         <div>
             <h1>Result</h1>
             <p>Requirement: {requirement}</p>
-            <p>Test Type: {testType}</p>
+            <p>Test Type: {testType === 'BDD' && 'BDD' || testType === 'TDD' && 'TDD'}</p>
+            {/* if test type = bdd then show bdd or show tdd if  = tdd */}
         </div>
     )
 }
