89aa93a36fd89b776e6c4c23d6bf9b8ae8fabcae
twentyvanty <lovelyzkim4@gmail.com>
2026-01-22 15:19:21 +0700

    fix bug

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,35 @@
-import React from 'react'
-import ReactDom from 'react-dom/client'
-import { BrowserRouter } from "react-router-dom";
+// import React from 'react'
+// import ReactDom from 'react-dom/client'
+// import { BrowserRouter } from "react-router-dom";
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import './App.css'
+// import App from './App.tsx'
+
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <BrowserRouter>
+//     <App />
+//     </BrowserRouter>
+//   </StrictMode>,
+// )
+// ReactDom.createRoot(document.getElementById('root')!).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// )
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
 import './index.css'
 import './App.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-    <App />
+      <App />
     </BrowserRouter>
-  </StrictMode>,
-)
-ReactDom.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  </StrictMode>
 )
