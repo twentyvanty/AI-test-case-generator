fdbbb9bcff592f19c03cc4d8b35556f3883526f3
twentyvanty <lovelyzkim4@gmail.com>
2026-01-22 14:58:19 +0700

    fixed a  bug

---
+++ AI-test-case-generator-main/src/main.tsx
@@ -0,0 +1,29 @@
+// import React from 'react'
+// import ReactDom from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import './App.css'
+import App from './App.tsx'
+
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <BrowserRouter>
+//     <App />
+//     </BrowserRouter>
+//   </StrictMode>,
+// )
+// ReactDom.createRoot(document.getElementById('root')!).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// )
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+)
